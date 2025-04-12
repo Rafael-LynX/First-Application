0eb6779725b55a2b49ff49e92724971506f866d5
Rafael Vieira <109623407+Rafael-LynX@users.noreply.github.com>
2025-04-12 16:08:32 -0300

    v6

--- src/app/App.java
+++ src/app/App.java
@@ -3,12 +3,12 @@
 package src.app;
 import java.util.logging.Logger;
 
+
 public class App {
     Logger logger = Logger.getLogger(getClass().getName());
     public App() {
         logger.info("Aplicação de Banco Iniciada!");
-            
-    
+
             
     }
 }
